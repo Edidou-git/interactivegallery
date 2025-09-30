11ddbbebc1e5912cad172efcc76207d347a31b71
Edidou-git <nganzimraissa746@gmail.com>
2025-09-30 12:59:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+/* Calculatrice!
+const valA = Number (prompt("veuillez saisir la valeur de A:"));
+const valB= Number (prompt ('veuillez saisir la valeur de B'));
+const sum = valA / valB;
+alert(valA + "/" + valB + "=" + sum);*/
+// créer 
+//et Nombre = Number(prompt("Entrez un nombre"));
+//let operator = Number  (prompt("Entrez un signe d'opération"));
+//let nombre = Number(prompt("Entrez un nombre"));
+//let result
+//if(operator==='+'){
+  //  result = Nombre + nombre"
+    //alert("cvcv")
+//}
+//else if (operator ==+++P°LPKFO?
+/*const user = {
+    firstName : "Raissa",
+    lastName : "Nganzim",
+    gender : "F",
+    age : 19
+    birthdate : "15/11/2005,
+}*/
+const user ={};
+
+user.old= prompt("Votre age");
+user.firstname = prompt("Votre prénom svp");
+if (user.old<18){ alert(user.firstname +" Vous êtes mineur!")
+}
+while(user.old.length<=0)
+else{alert(user.firstname +" Vous êtes Majeur!");
+
+}
